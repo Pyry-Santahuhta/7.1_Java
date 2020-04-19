d9f6448e71646b71fccccee487a3aec26a7b2c78
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2020-04-19 10:39:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firstbutton/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.firstbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public  void testMethod(View v){
+        System.out.println("Hello world!");
+    }
+
+
+}
+
+
+
+
+
+
+
+
